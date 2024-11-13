5a93e656634689141c095030447480a5315df233
鈴木愛海 <>
2024-11-13 19:46:08 +0900

    pry byebug を導入

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,12 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update]
 
     def index
+
       @articles = Article.all
     end
 
     def show
-     
+
     end
 
     def new
@@ -24,11 +25,11 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-      
+
     end
 
     def update
-      
+
       if @article.update(article_params)
         redirect_to article_path(@article),notice: '更新できました'
       else
@@ -42,14 +43,11 @@ class ArticlesController < ApplicationController
       article.destroy!
       redirect_to root_path, notice: '削除に成功しました'
     end
-  
-  
+
     private
     def article_params
       params.require(:article).permit(:title, :content)
     end
-
-  
 
 def set_article
   @article = Article.find(params[:id])
