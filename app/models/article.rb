7ae2ab1083476eeb99a24076f114cd5a401b413e
鈴木愛海 <>
2025-03-20 13:53:01 +0900

    action text の設定

--- app/models/article.rb
+++ app/models/article.rb
@@ -21,10 +21,9 @@ class Article < ApplicationRecord
 
   #length
   validates :title, length:{minimum: 2, maxmum: 100}
-  validates :content, length:{minimum: 10}
-  validates :content, uniqueness: true
+
   validates :title, format: {with: /\A(?!\@)/ }
-  validate  :validate_title_and_content_length
+
 
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
@@ -42,14 +41,4 @@ class Article < ApplicationRecord
   def like_count
     likes.count
   end
-
-
-  private
-  def validate_title_and_content_length
-    char_count = self.title.length + self.content.length
-    unless char_count > 100
-      errors.add(:content, '100文字以上で')
-    end
-  end
-
 end
