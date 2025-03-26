442f1d87478051d000c62c97241dc75aa9a5378b
鈴木愛海 <>
2025-03-26 17:11:10 +0900

    いいねの状態をjQueryで反映

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -42,11 +42,15 @@ document.addEventListener('turbolinks:load', () => {
       const hasLiked = response.data.hasLiked
       handleHeartDisplay(hasLiked)
     })
-    
+
     $('.inactive-heart').on('click', () => {
       axios.post(`/articles/${articleId}/like`)
         .then((response) => {
-          console.log(response)
+          if (response.data.status === 'ok') {
+            $('.active-heart').removeClass('hidden')
+            $('.inactive-heart').addClass('hidden')
+          }
+
         })
         .catch((e) => {
           window.alert('Error')
@@ -57,7 +61,10 @@ document.addEventListener('turbolinks:load', () => {
     $('.active-heart').on('click', () => {
       axios.delete(`/articles/${articleId}/like`)
         .then((response) => {
-          console.log(response)
+          if (response.data.status === 'ok') { 
+            $('.active-heart').addClass('hidden')
+            $('.inactive-heart').removeClass('hidden') 
+          }
         })
         .catch((e) => {
           window.alert('Error')
